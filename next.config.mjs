1af009acdcee6451b7b8b7cc6e857ed59617b42a
Sergio <sergio@MacBook-Pro.local>
2024-07-31 14:18:01 -0700

    feat: update setings project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_LOCAL: process.env.NEXT_PUBLIC_API_LOCAL,
+  },
+};
 
 export default nextConfig;
