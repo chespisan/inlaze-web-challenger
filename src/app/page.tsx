1c6ceac79d9f3d57e81d0002b88ef2e7edc8889f
Sergio <sergio@MacBook-Pro.local>
2024-08-01 12:33:50 -0700

    log:  logs - test v0.12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
-import HomePage from "app/pages/home";
-
 export default async function Home() {
   const api = process.env.NEXT_PUBLIC_API_LOCAL;
+  // const api2 = process.env.API_LOCAL;
+  console.log("api: ", api);
+  // console.log("api2: ", api2);
   let productsFake = [];
-
   try {
     const response = await fetch(`${api}/api`);
-    console.log("response", response);
     const { products } = await response.json();
-
     console.log("Products: ", products);
-    // productsFake = products;
+    productsFake = products;
   } catch (error) {
     console.log("error: ", error);
   }
-  // return <HomePage movies={movies} genres={genres} />;
+
   return (
     <div>
-      <h1>Logs Test</h1>
+      <ul>
+        {productsFake?.map((product: any) => (
+          <li key={product?.id}>{product?.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
