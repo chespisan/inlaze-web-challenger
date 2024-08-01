64c65a4c1ccb748c10e393716a129a48940918d7
Sergio <sergio@MacBook-Pro.local>
2024-08-01 11:26:06 -0700

    log:  logs - test v0.6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 import HomePage from "app/pages/home";
 
 const Home = async () => {
-  const api = process.env.NEXT_PUBLIC_API_LOCAL;
-  console.log("apiLocal: ", api);
+  try {
+    const api = process.env.NEXT_PUBLIC_API_LOCAL;
+    console.log("apiLocal: ", api);
 
-  const response = await fetch(`${api}/api`);
-  const { movies } = await response.json();
-  console.log("movies: ", movies);
+    const response = await fetch(`${api}/api`);
+    console.log("response: ", response);
+    const { movies } = await response.json();
+    console.log("movies: ", movies);
 
-  const responseGenres = await fetch(`${api}/api/genres`);
-  const { genres } = await responseGenres.json();
-  console.log("genres: ", genres);
-
-  return <HomePage movies={movies} genres={genres} />;
+    const responseGenres = await fetch(`${api}/api/genres`);
+    console.log("responseGenres: ", responseGenres);
+    const { genres } = await responseGenres.json();
+    console.log("genres: ", genres);
+    return <HomePage movies={movies} genres={genres} />;
+  } catch (error) {
+    console.log("error: ", error);
+  }
 };
 
 export default Home;
