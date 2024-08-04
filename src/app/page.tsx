398e5b6c9c9abeb35fbbf59be5d68ec500e15bc5
Sergio <sergio@MacBook-Pro.local>
2024-08-04 14:48:14 -0700

    log:  logs - test v0.20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 export default async function Home() {
   const api = process.env.NEXT_PUBLIC_API_LOCAL;
-  // const api2 = process.env.API_LOCAL;
-  console.log("api: ", api);
-  // console.log("api2: ", api2);
+  // // const api2 = process.env.API_LOCAL;
+  // console.log("api: ", api);
+  // // console.log("api2: ", api2);
   let productsFake: any = [];
-  try {
-    const response = await fetch(`${api}/api`);
-    console.log("response: ", response);
+  // try {
+  //   const response = await fetch(`${api}/api`);
+  //   console.log("response: ", response);
 
-    const data = await response.json();
-    console.log("Products: ", data);
-    // productsFake = products;
-  } catch (error) {
-    console.log("error: ", error);
-  }
+  //   const data = await response.json();
+  //   console.log("Products: ", data);
+  //   // productsFake = products;
+  // } catch (error) {
+  //   console.log("error: ", error);
+  // }
 
   return (
     <div>
