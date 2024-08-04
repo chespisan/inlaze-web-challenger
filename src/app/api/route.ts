1a817e0b3d4b5408360178cd35d2490d2330e964
Sergio <sergio@MacBook-Pro.local>
2024-08-04 14:07:12 -0700

    log:  logs - test v0.16

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,8 @@
+import { FakeProductsService } from "app/services/fakeProducts";
+
+const fakleProductsService = new FakeProductsService();
+
 export async function GET() {
-  return [
-    { id: 1, name: "Product 1", price: 10 },
-    { id: 2, name: "Product 2", price: 20 },
-    { id: 3, name: "Product 3", price: 30 },
-  ];
+  const products = await fakleProductsService.getProducts();
+  return Response.json({ products });
 }
