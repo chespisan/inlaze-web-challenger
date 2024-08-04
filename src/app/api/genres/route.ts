863a60a4c4665e09ce93641a5546b920735d6b1c
Sergio <sergio@MacBook-Pro.local>
2024-08-04 12:54:51 -0700

    log:  logs - test v0.13

--- src/app/api/genres/route.ts
+++
@@ -1,8 +0,0 @@
-import { MoviesService } from "app/services/movies";
-
-const moviesService = new MoviesService();
-
-export async function GET() {
-  const genres = await moviesService.getGenreMovies();
-  return Response.json({ genres });
-}
