bed4f2fc8385d78550ceb3a4af41a8b7051c1a14
Sergio <sergio@MacBook-Pro.local>
2024-08-04 14:34:29 -0700

    log:  logs - test v0.17

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    API_LOCAL: process.env.NEXT_PUBLIC_API_LOCAL,
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
